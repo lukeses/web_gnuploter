4b9cdf51aac92e33bf46f07aa81c13d0678b616b
Lukasz Jankowski <lukaszpjankowski@gmail.com>
2015-03-10 00:33:10 +0100

    showing chart in application

--- app/models/chart.rb
+++ app/models/chart.rb
@@ -1,5 +1,5 @@
 class Chart < ActiveRecord::Base
 
-  has_attached_file :photo
+  has_attached_file :photo, :default_url => 'missing.png'
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
